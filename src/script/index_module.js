3422d5fd1dc2b2d947390f18923049891a47ae26
xijiajie <1054530015@qq.com>
2020-12-14 23:08:49 +0800

    12.14提交

--- src/script/index_module.js
+++ src/script/index_module.js
@@ -1,100 +1,219 @@
-// define(['jcookie'], () => {
-//     return {
-//         init: function() {
-//             let $sid = location.search.substring(1).split('=')[1]; //得到side 4
-//             if (!$sid) { //没有值 输出乱七八糟的东西 不存在为假
-//                 $sid = 1;
-//             }
-//             //2.将sid传给后端，后端根据对应的sid返回不同的数据。
-//             $.ajax({
-//                 url: 'http://localhost/dashboard/JS2010/week06/Day%2029-Day%2031_jquery/projectname/php/detail.php',
-//                 data: {
-//                     sid: $sid
-//                 },
-//                 dataType: 'json'
-//             }).done(function(data) { //后端php页面根据sid找到数据库对应那条数据
-//                 console.log(data);
-//                 console.log(data.urls);
-//                 //获取数据，将数据放入对应的结构中。
-//                 $('#smallpic').attr('src', data.url);
-//                 $('.loadtitle').html(data.title);
-//                 $('.loadpcp').html(data.price);
-
-//                 //渲染放大镜下面的小图
-//                 let $picurl = data.urls.split(','); //将数据转换成数组。
-//                 let $strhtml = '';
-//                 const $list = $('#list');
-//                 console.log($picurl);
-//                 $.each($picurl, function(index, value) {
-//                     $strhtml += `
-//                         <li>
-//                             <img src="${value}"/>
-//                         </li>
-//                     `;
-//                 });
-//                 $list.html($strhtml);
-//             });
-//             //五.购物车：(商品sid、商品数量)
-//             //1.设置存储cookie的变量。
-//             let arrsid = []; //存储商品的sid
-//             let arrnum = []; //存储商品的数量
-//             //2.判断是第一次存储，多次存储。
-//             //获取cookie才能判断，每存储一个商品对应的cookie就会发生变化。
-//             //提前预判cookie设置时的key值(cookiesid/cookienum)
-//             function getcookietoarray() {
-//                 if ($.cookie('cookiesid') && $.cookie('cookienum')) {
-//                     arrsid = $.cookie('cookiesid').split(',');
-//                     arrnum = $.cookie('cookienum').split(',');
-//                 }
-//             }
-//             //上面的函数获取cookie值，并且转换成数组，方便判断是否是第一次。
-//             //第一次存储添加sid进入arrsid，存储数量
-//             //第二次以上，直接修改数量。
-//             $('.p-btn a').on('click', function() {
-//                 getcookietoarray(); //获取cookie，变成数组，判断是否存在。
-//                 if ($.inArray($sid, arrsid) === -1) { //不存在
-//                     arrsid.push($sid);
-//                     $.cookie('cookiesid', arrsid, 10);
-//                     arrnum.push($('#count').val());
-//                     $.cookie('cookienum', arrnum, 10);
-//                 } else { //存着
-//                     //通过$sid获取商品的数量所在的位置。
-//                     let $index = $.inArray($sid, arrsid);
-//                     // arrnum[$index]//原来的数组
-//                     // $('#count').val()//新添加的数量
-//                     arrnum[$index] = parseInt(arrnum[$index]) + parseInt($('#count').val()); //重新赋值
-//                     $.cookie('cookienum', arrnum, { expires: 10, path: '/' });
-//                 }
-//                 alert('按钮被点击了');
-//             });
-//         }
-//     }
-// });
-define(['jlazyload'], () => { //懒加载依赖与这个
+define(['jlazyload'], () => {
     return {
         init: function() {
-            const $list = $('.list123');
+            //代码实现
+            //1.鼠标移入左侧的li元素，显示右侧的大盒子。
+            const $list = $('.menu li');
+            const $cartlist = $('.cartlist');
+            const $items = $('.item');
+            $list.hover(function() {
+                $cartlist.show();
+                $(this).siblings('li').removeClass('selected');
+                $(this).addClass('selected');
+                $(this).addClass('active').siblings('li').removeClass('active');
+                //切换内容发生改变，不同的li对应不同的内容块。
+                $items.eq($(this).index()).show().siblings('.item').hide();
+            });
+            //2.鼠标移入右侧的大盒子，大盒子依然显示隐藏
+            $cartlist.hover(function() {
+                $(this).show();
+            }, function() {
+                $(this).hide();
+            });
+
+            //渲染数据第一块运动户外
+            const $list2 = $('.list1234');
             $.ajax({
                 url: 'http://localhost/dashboard/kaola/php/index.php',
                 dataType: 'json'
             }).done(function(data) { //一个页面对应所有的数据接口
                 let $strhtml = '';
                 $.each(data, function(index, value) {
-                    $strhtml += `
-                    <li class="s1-li">
-                        <h3 class="tit1">${value.title}</h3>
-                            <p class="s1-into">${value.intro}</p>
-                            <img class="img-lazyload u-loaded" src="${value.url}">
-                    </li>
+                    if (index < 4) {
+                        $strhtml += `
+                        <li class="s1-li">
+                                <h3 class="tit1">${value.title}</h3>
+                                <p class="s1-into">${value.intro}</p>
+                                <img class="img-lazyload u-loaded" src="${value.url}">
+                        </li>
                     `;
+                    }
                 });
-                $list.html($strhtml);
+                $list2.html($strhtml);
                 $(function() { //页面加载完成
                     $("img.lazy").lazyload({
                         effect: "fadeIn" //显示方法：淡入
                     });
                 });
             });
+
+            // //渲染数据第二块美妆专区
+            const $list3 = $('.list5678');
+            $.ajax({
+                url: 'http://localhost/dashboard/kaola/php/index.php',
+                dataType: 'json'
+            }).done(function(data) { //一个页面对应所有的数据接口
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    if (index >= 4 && index <= 24) {
+                        //因为之前删掉了数据sid。这里的4-24其实就是sid21-24
+                        $strhtml += `
+                        <li class="s1-li">
+                            <h3 class="tit1">${value.title}</h3>
+                            <p class="s1-into">${value.intro}</p>
+                            <img class="img-lazyload u-loaded" src="${value.url}">
+                        </li>
+                        `;
+                    }
+                });
+                $list3.html($strhtml);
+                $(function() { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //显示方法：淡入
+                    });
+                });
+            });
+
+            //轮播
+            const $lunbo = $('.content'); //全部包起来的
+            const $ulist = $('.content ul'); //运动的盒子
+            const $picList = $('.content ul li'); //3个图片
+            const $btnList = $('.content ol li'); //3个圈圈
+            const $left = $('#left');
+            const $right = $('#right');
+            let timer = null;
+            let $num = 0; //存储索引值   
+            //鼠标移入小圆圈的函数
+            function lunbo() {
+                $picList.css({ "display": "none" });
+                // $(this).index():当前移入小点的索引
+                // 将移入小点的对应的图片显示出来,并且将对应的小点的样式改掉
+                $btnList.eq($num).addClass('stay').siblings().removeClass('stay');
+                $picList.eq($num).css({ "display": "block" });
+            }
+            // 移入对应的小点，显示对应的图片
+            $btnList.on('mouseover', function() {
+                console.log($(this).index());
+                // 在找之前将所有的图片隐藏
+                // $picList.css({
+                //     "display": "none"
+                // });
+                // // $(this).index():当前移入小点的索引就是图片的索引
+                // // 将移入小点的对应的图片显示出来,并且将对应的小点的样式改掉
+                // $btnList.eq($(this).index()).addClass('active').siblings().removeClass('active');
+                // $picList.eq($(this).index()).css({
+                //     "display": "block"
+                // });
+
+
+                // 将当前索引给$num
+                $num = $(this).index();
+                lunbo();
+            });
+
+            // 将向右轮播封装
+            function rightfun() {
+                $num++;
+                if ($num > $btnList.length - 1) {
+                    $num = 0;
+                }
+                lunbo();
+            }
+
+            // 点击右箭头，图片向右轮播
+            $right.on('click', function() {
+                // $num++;
+                // if ($num > $btnList.length - 1) {
+                //     $num = 0;
+                // }
+                // $picList.css({
+                //     "display": "none"
+                // });
+                // $btnList.eq($num).addClass('active').siblings().removeClass('active');
+                // $picList.eq($num).css({
+                //     "display": "block"
+                // });
+                // lunbo();
+                rightfun();
+            });
+
+            // 点击左箭头，图片向左轮播
+            $left.on('click', function() {
+                $num--;
+                if ($num < 0) {
+                    $num = $btnList.length - 1;
+                }
+                // $picList.css({
+                //     "display": "none"
+                // });
+                // $btnList.eq($num).addClass('active').siblings().removeClass('active');
+                // $picList.eq($num).css({
+                //     "display": "block"
+                // });
+                lunbo();
+            });
+
+            // 定时器
+            $timer = setInterval(function() {
+                rightfun();
+            }, 3000);
+
+            // 移入最大的盒子，停止定时器
+            $lunbo.on('mouseover', function() {
+                clearInterval($timer);
+            })
+
+            // 移出盒子，开启定时器
+            $lunbo.on('mouseout', function() {
+                $timer = setInterval(function() {
+                    rightfun();
+                }, 3000);
+            })
+
+            //商城顶部悬浮
+            const $topxuanfu = $('.searchflex');
+            $(window).on('scroll', () => {
+                let $scrolltop = $(window).scrollTop(); //获取滚动条top值
+                if ($scrolltop >= 40) {
+                    // clearInterval(timer);
+                    console.log($scrolltop);
+                    $topxuanfu.css({
+                        // top: 0 //改变topxuanfu的top值参数 就是定位的那个参数
+                        "position": "fixed",
+                        "top": 0,
+                        "display": "block"
+                    })
+                } else {
+                    $topxuanfu.css({
+                        // top: -60
+                        "display": "none"
+                    })
+                }
+            });
+
+
+            // 侧边栏悬浮
+
+            const $aside = $('.loutinav');
+            $(window).on('scroll', () => {
+                let $scrolltop = $(window).scrollTop(); //获取滚动条top值
+                if ($scrolltop >= 636) {
+                    // console.log($scrolltop);
+                    $aside.css({
+                        "position": "fixed",
+                        "top": "66px",
+                        "left": "50%",
+                        "margin-left": "-665px"
+                    })
+                } else {
+                    $aside.css({
+                        "position": "absolute",
+                        "top": 0,
+                        "left": "-120px",
+                        "margin-left": 0
+                    })
+                }
+            });
         }
     }
-})
+});
